08756105c67aa9d497e2a0eb0d529e4ffb8269d1
holyang071620 <obinakelle@gmail.com>
2024-11-19 11:53:15 +0800

    Initial setup with Express server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    res.send('Render Deployment Project');
+
+});
+
+app.listen(port, () => {
+    console.loh(`Server running on http://localhost:${port}`);
+
+});
